6ae50ad3a2830aa064f958bbfb5cd61bd7e1ea33
Syed Hashir Ahmad <66515282+Hashirahmad996@users.noreply.github.com>
2025-08-24 23:59:04 +0500

    Update server.js
    add3

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-require('dotenv').config({ path: './app.env' });
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.use('/', routes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
