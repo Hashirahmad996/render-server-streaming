2b83f59d0fabdf8e755409a8f8e0483a655abf80
Syed Hashir Ahmad <66515282+Hashirahmad996@users.noreply.github.com>
2025-08-24 22:12:52 +0500

    Update index.js
    add

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const deploymentController = require('../controllers/deploymentController');
 
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Render Server Streaming API' });
+});
+
 router.get('/deployments/:id/stream', deploymentController.streamDeployment);
 
 module.exports = router;
