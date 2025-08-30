e1db1c4f7567031b6038eb68c5cb459891001158
Syed Hashir Ahmad <66515282+Hashirahmad996@users.noreply.github.com>
2025-08-30 16:05:38 +0500

    Update pusherService.js
    update file

--- services/pusherService.js
+++ services/pusherService.js
@@ -12,28 +12,15 @@ const pusher = new Pusher({
  * Call this function multiple times during your deployment 
  * to send real-time log messages to the frontend.
  */
-exports.sendLogUpdate = async (logMessage) => {
-  const data = { message: logMessage };
-  await pusher.trigger('my-channel', 'log-update', data);
-};
+exports.sendLogUpdate = async (logMessage) => { await pusher.trigger('my-channel', 'log-update', data); };
 
 /**
  * Call this function if the deployment fails at any point.
  */
-exports.sendError = async (errorMessage) => {
-  const data = { error: errorMessage };
-  await pusher.trigger('my-channel', 'deployment-error', data);
-};
+exports.sendError = async (errorMessage) => { await pusher.trigger('my-channel', 'deployment-error', data); };
 
 /**
  * Call this function only once at the very end when the 
  * deployment has completed successfully.
  */
-exports.sendSuccess = async (appUrl, monitorUrl) => {
-  const data = {
-    app_url: appUrl,
-    monitor_url: monitorUrl,
-  };
-  await pusher.trigger('my-channel', 'deployment-success', data);
-};
-
+exports.sendStatusUpdate = async (data) => { await pusher.trigger('my-channel', 'deployment-success', data); };
