8ccb36cb2a5b350e499faf2c40efaf5519ee6c69
Syed Hashir Ahmad <66515282+Hashirahmad996@users.noreply.github.com>
2025-08-30 15:27:26 +0500

    Update pusherService.js
    add more logic

--- services/pusherService.js
+++ services/pusherService.js
@@ -8,6 +8,32 @@ const pusher = new Pusher({
   useTLS: true,
 });
 
-exports.sendStatusUpdate = async (data) => {
-  await pusher.trigger('my-channel', 'my-event', data);
+/**
+ * Call this function multiple times during your deployment 
+ * to send real-time log messages to the frontend.
+ */
+exports.sendLogUpdate = async (logMessage) => {
+  const data = { message: logMessage };
+  await pusher.trigger('my-channel', 'log-update', data);
 };
+
+/**
+ * Call this function if the deployment fails at any point.
+ */
+exports.sendError = async (errorMessage) => {
+  const data = { error: errorMessage };
+  await pusher.trigger('my-channel', 'deployment-error', data);
+};
+
+/**
+ * Call this function only once at the very end when the 
+ * deployment has completed successfully.
+ */
+exports.sendSuccess = async (appUrl, monitorUrl) => {
+  const data = {
+    app_url: appUrl,
+    monitor_url: monitorUrl,
+  };
+  await pusher.trigger('my-channel', 'deployment-success', data);
+};
+
